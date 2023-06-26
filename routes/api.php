940d416556181e3a2e7b8f78df670d011aaaf581
GiacomoArena <giacomoarena28@gmail.com>
2023-06-26 16:14:05 +0200

    make api controller and json file on thunderclient

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PortfolioController;
+use App\Models\Portfolio;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::namespace('Api')
+                ->prefix('portfolios')
+                ->group(function(){
+                    Route::get('/', [PortfolioController::class, 'index']);
+                });
