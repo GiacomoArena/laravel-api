413acb8d38f8fe1b788b582ad934cb460200a7f8
GiacomoArena <giacomoarena28@gmail.com>
2023-06-28 18:11:42 +0200

    make controllers

--- app/Http/Controllers/Api/PortfolioController.php
+++ app/Http/Controllers/Api/PortfolioController.php
@@ -5,13 +5,46 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
+use App\Models\Type;
+use App\Models\Technology;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class PortfolioController extends Controller
 {
     public function index (){
-        $portfolios = Portfolio::all();
+        $portfolios = Portfolio::with('type', 'technologies')->paginate(10);
 
         return response()->json($portfolios);
+}
+    public function getDetail ($slug){
+        $portfolios = Portfolio::where('slug', $slug)->with('type', 'technologies')->first();
+
+        return response()->json($portfolios);
+}
+    public function get_by_type ($id){
+        $portfolios = Portfolio::where('type_id', $id)->with('type', 'technologies')->paginate(10);
+
+        return response()->json($portfolios);
+}
+    public function get_by_tech ($id){
+        $portfolios = Portfolio::with('type', 'technologies')
+                    ->whereHas('technologies', function(Builder $query) use($id){
+                        $query->where('technology_id',$id);
+                    })->paginate(10);
+
+        return response()->json($portfolios);
+}
+
+    public function type (){
+        $type = Type::all();
+
+        return response()->json($type);
+}
+    public function tech (){
+        $technologies = Technology::all();
+
+        return response()->json($technologies);
 }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,9 @@ Route::namespace('Api')
                 ->prefix('portfolios')
                 ->group(function(){
                     Route::get('/', [PortfolioController::class, 'index']);
+                    Route::get('/type', [PortfolioController::class, 'type']);
+                    Route::get('/tech', [PortfolioController::class, 'tech']);
+                    Route::get('/{slug}', [PortfolioController::class, 'getDetail']);
+                    Route::get('/by-type/{id}', [PortfolioController::class, 'get_by_type']);
+                    Route::get('/by-tech/{id}', [PortfolioController::class, 'get_by_tech']);
                 });
